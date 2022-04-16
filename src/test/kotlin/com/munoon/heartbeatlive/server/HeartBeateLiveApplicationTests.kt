b1b0de41edff5a6e073157c1f7fcb2217322ba75
munoon <munoongg@gmail.com>
2022-04-16 19:02:51 +0300

    Initial commit

---
+++ src/test/kotlin/com/munoon/heartbeatlive/server/HeartBeateLiveApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.munoon.heartbeatlive.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HeartBeateLiveApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
