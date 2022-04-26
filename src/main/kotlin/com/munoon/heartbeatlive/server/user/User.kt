b7a216b9e058a8347babe18920e90c8631f7b2d8
Nikita Ivchenko <munoongg@gmail.com>
2022-04-26 13:15:46 +0300

    Make User#email field unique in MongoDB

--- src/main/kotlin/com/munoon/heartbeatlive/server/user/User.kt
+++ src/main/kotlin/com/munoon/heartbeatlive/server/user/User.kt
@@ -1,16 +1,18 @@
 package com.munoon.heartbeatlive.server.user
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.Instant
 
-@Document
+@Document("users")
 data class User(
     @Id
     val id: String,
 
     val displayName: String?,
 
+    @Indexed(unique = true)
     val email: String?,
 
     val emailVerified: Boolean,
@@ -18,4 +20,8 @@ data class User(
     val created: Instant = Instant.now(), // TODO change?
 
     val roles: Set<UserRole> = emptySet()
-)
+) {
+    companion object {
+        const val UNIQUE_EMAIL_INDEX = "user_unique_email_index"
+    }
+}
--- src/test/kotlin/com/munoon/heartbeatlive/server/AbstractMongoDBTest.kt
+++ src/test/kotlin/com/munoon/heartbeatlive/server/AbstractMongoDBTest.kt
@@ -8,32 +8,43 @@ import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.BeforeEach
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Import
+import org.springframework.context.event.ContextRefreshedEvent
+import org.springframework.context.event.EventListener
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate
 import reactor.kotlin.core.publisher.toMono
 
 @AutoConfigureDataMongo
+@Import(AbstractMongoDBTest.TestMongoConfig::class)
 abstract class AbstractMongoDBTest : AbstractTest() {
     @Autowired
     private lateinit var mongoTemplate: ReactiveMongoTemplate
 
-    @BeforeEach
-    fun createMongoUserIfNotExist() {
-        runBlocking {
-            val mongoClient = MongoClients.create("mongodb://localhost:27017")
-            val adminDb = mongoClient.getDatabase("admin")
-            val userName = "mongodb"
+    @TestConfiguration
+    class TestMongoConfig {
+        @Order(Ordered.HIGHEST_PRECEDENCE)
+        @EventListener(ContextRefreshedEvent::class)
+        fun createMongoUserIfNotExist() {
+            runBlocking {
+                val mongoClient = MongoClients.create("mongodb://localhost:27017")
+                val adminDb = mongoClient.getDatabase("admin")
+                val userName = "mongodb"
 
-            val userExist = adminDb.getCollection("system.users")
-                .countDocuments(BasicDBObject(mapOf("user" to userName)))
-                .awaitSingle() > 0
+                val userExist = adminDb.getCollection("system.users")
+                    .countDocuments(BasicDBObject(mapOf("user" to userName)))
+                    .awaitSingle() > 0
 
-            if (!userExist) {
-                val createUserCommand = BasicDBObject(mapOf(
-                    "createUser" to userName,
-                    "pwd" to "password",
-                    "roles" to arrayOf("readWrite")
-                ))
-                adminDb.runCommand(createUserCommand).awaitFirstOrNull()
+                if (!userExist) {
+                    val createUserCommand = BasicDBObject(mapOf(
+                        "createUser" to userName,
+                        "pwd" to "password",
+                        "roles" to arrayOf("readWrite")
+                    ))
+                    adminDb.runCommand(createUserCommand).awaitFirstOrNull()
+                }
             }
         }
     }
