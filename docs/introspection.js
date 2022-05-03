0760da08cdd1cc1287b47b2668215c32f975eb76
Nikita Ivchenko <munoongg@gmail.com>
2022-05-03 23:35:42 +0300

    Delete docs/introspection.js, refactor it with spectaql config

--- docs/introspection.js
+++
@@ -1,9 +0,0 @@
-const graphql = require("graphql");
-const loadFiles = require("@graphql-tools/load-files");
-const merge = require("@graphql-tools/merge");
-
-const files = loadFiles.loadFilesSync("../src/main/resources/graphql");
-const mergeTypeDefs = merge.mergeTypeDefs(files);
-const printedTypeDefs = graphql.print(mergeTypeDefs);
-const schema = graphql.buildSchema(printedTypeDefs);
-module.exports = graphql.introspectionFromSchema(schema);
